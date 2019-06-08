c7f41846af700045ac544e97ebe00779b87b36fa
Guilherme <pereiraguilherme997@gmail.com>
2019-06-08 00:09:43 -0300

    Refactor send_functions for further code

--- send_functions.py
+++ send_functions.py
@@ -21,6 +21,12 @@ def checksum(msg):
 	return s
 
 
+"""
+Function that build ipv6 header
+	@paramter source_addr - source address (check ifconfig)
+	@paramter dest_addr - destination address
+	@return ipv6 header in hexadecimal
+"""
 def build_ipv6_header(source_addr, dest_addr):
 	version = 6
 	traffic_class = 0
@@ -37,27 +43,16 @@ def build_ipv6_header(source_addr, dest_addr):
 	ip_header = pack('!II' , sub_packet_1,sub_packet_2,source_addr,dest_addr)
 	return ip_header
 
-
-if __name__ == "__main__":
-	# src=fe:ed:fa:ce:be:ef, dst=52:54:00:12:35:02, type=0x0800 (IP)
-	dst_mac = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
-	src_mac = [0x00, 0x0a, 0x11, 0x11, 0x22, 0x22]
-	
-	# Ethernet header
-	eth_header = pack('!6B6BH', dst_mac[0], dst_mac[1], dst_mac[2], dst_mac[3], dst_mac[4], dst_mac[5], 
-		src_mac[0], src_mac[1], src_mac[2], src_mac[3], src_mac[4], src_mac[5], 0x86DD)
-	source_ip = '10.32.143.171 '
-	dest_ip = '10.32.143.171 '			# or socket.gethostbyname('www.google.com')
-
-	
-	source_addr = socket.inet_pton(socket.AF_INET6,"fe80::a61f:72ff:fef5:9086")
-	dest_addr = socket.inet_pton(socket.AF_INET6,"fe80::a61f:72ff:fef5:9086")
-	
-	ip_header = build_ipv6_header(source_addr, dest_addr)
-
+"""
+Function that build tcp header
+	@parameter source - source port 
+	@paramter dest - destination port 
+	@paramter source_ip - source ip (check ifconfig)
+	@parameter dest_ip - destination ip 
+	@return tcp header in hexadecimal
+"""
+def build_tcp_header(source, dest, source_ip, dest_ip):
 	# tcp header fields
-	source = 1234   # source port
-	dest = 33   # destination port
 	seq = 0
 	ack_seq = 0
 	doff = 5    #4 bit field, size of tcp header, 5 * 4 = 20 bytes
@@ -86,14 +81,34 @@ if __name__ == "__main__":
 	protocol = socket.IPPROTO_TCP
 	tcp_length = len(tcp_header)
 	 
-	psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
-	psh = psh + tcp_header;
+	psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length)
+	psh = psh + tcp_header
 	 
 	tcp_checksum = checksum(psh)
 	 
 	# make the tcp header again and fill the correct checksum
-	tcp_header = pack('!HHLLBBHHH' , source, dest, seq, ack_seq, offset_res, tcp_flags,  window, tcp_checksum , urg_ptr)
+	return pack('!HHLLBBHHH' , source, dest, seq, ack_seq, offset_res, tcp_flags,  window, tcp_checksum , urg_ptr)
 	 
+
+if __name__ == "__main__":
+	# src=fe:ed:fa:ce:be:ef, dst=52:54:00:12:35:02, type=0x0800 (IP)
+	dst_mac = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
+	src_mac = [0x00, 0x0a, 0x11, 0x11, 0x22, 0x22]
+	
+	# Ethernet header
+	eth_header = pack('!6B6BH', dst_mac[0], dst_mac[1], dst_mac[2], dst_mac[3], dst_mac[4], dst_mac[5], 
+		src_mac[0], src_mac[1], src_mac[2], src_mac[3], src_mac[4], src_mac[5], 0x86DD)
+	
+	source_ip = '10.32.143.171 '
+	dest_ip = '10.32.143.171 '			# or socket.gethostbyname('www.google.com')
+
+	
+	source_addr = socket.inet_pton(socket.AF_INET6,"fe80::a61f:72ff:fef5:9086")
+	dest_addr = socket.inet_pton(socket.AF_INET6,"fe80::a61f:72ff:fef5:9086")
+	
+	ip_header = build_ipv6_header(source_addr, dest_addr)
+	tcp_header = build_tcp_header(1234, 0, source_ip, dest_ip)
+
 	# final full packet - syn packets dont have any data
 	packet = eth_header + ip_header + tcp_header
 	r = sendeth(packet, "enp4s0")
